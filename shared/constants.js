ae9ba9ef956b818780b7caa1f17b18b58f253538
giacomo78-cmd <giacomo.menegus@icloud.com>
2025-11-29 22:54:21 +0100

    Create constants.js

---
+++ shared/constants.js
@@ -0,0 +1,8 @@
+// shared/schema.js
+// simple message types - for reference
+module.exports = {
+  CLIENT_JOIN: 'join',
+  CLIENT_INPUT: 'input',
+  SERVER_SNAPSHOT: 'snapshot',
+  SERVER_WELCOME: 'welcome'
+};
